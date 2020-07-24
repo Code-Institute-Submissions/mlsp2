d4e29fa5cf8201b3511adf59bca010a2976a5d95
gaspar91 <andreiafg.91@gmail.com>
2020-07-24 12:24:19 +0000

    added a variable and a function that closes the modal whenever we click on the x

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,7 +1,10 @@
 var modalButton = document.querySelector('.modal-button');
-
 var modalBg = document.querySelector('.modal-bg');
+var modalClose = document.querySelector('.modal-close');
 
 modalButton.addEventListener('click',function(){
     modalBg.classList.add('bg-active');
-})
+});
+modalClose.addEventListener('click',function(){
+    modalBg.classList.remove('bg-active');
+});
