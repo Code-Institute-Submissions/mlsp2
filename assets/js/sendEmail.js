d0e3292ad20c437bb598b54c45c5182bb8425dfe
gaspar91 <andreiafg.91@gmail.com>
2020-08-18 08:06:50 +0000

    deleted unnecessary code

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -4,26 +4,5 @@ export const sendEmail = (name,email) => {
     emailjs.send("gmail", "yourjourney", {
         "from_name": name,
         "from_email": email,
-    })
-        .then(
-            function (response) {
-                swal({
-                    title: "Good job!",
-                    text: "You're subscriptions has been registered",
-                    icon: "success",
-                    button: "Awesome!",
-                });
-                
-            },
-            function (error) {
-                swal({
-                    title: "Error!",
-                    text: "Please check fields",
-                    icon: "warning",
-                    button: "Ok, thank you",
-                }); 
-                
-            }
-
-        );
+    });
 };
